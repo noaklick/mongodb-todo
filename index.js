5d6c9b75d24d184ef7adb9cdce112aa8ec917213
Noa Kligfeld <noakligfeld@college.harvard.edu>
2023-02-13 17:00:36 -0500

    tutorial should be done

--- index.js
+++ index.js
@@ -1,23 +1,74 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// models
+const TodoTask = require("./models/TodoTask");
+
+
 dotenv.config();
 
 app.use("/static", express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 
+
+//connection to db
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+    console.log("Connected to db!");
+    app.listen(3000, () => console.log("Server Up and running"));
+});
+
+
+
 app.set("view engine", "ejs");
 
 // get method
-app.get('/',(req, res) => {
-    // example of template rendering concept
-    res.render('todo.ejs');
+app.get("/", (req, res) => {
+    TodoTask.find({}, (err, tasks) => {
+        res.render("todo.ejs", { todoTasks: tasks });
+    });
 });
 
 // post method
-app.post('/',(req, res) => {
-    console.log(req.body);
+app.post('/',async (req, res) => {
+    const todoTask = new TodoTask({
+        content: req.body.content
+    });
+    try {
+        await todoTask.save();
+        res.redirect("/");
+    } 
+    catch (err) {
+        res.redirect("/");
+    }
 });
 
-app.listen(3000, () => console.log("Server Up and running"));
+// update method
+//UPDATE
+app
+    .route("/edit/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.find({}, (err, tasks) => {
+            res.render("todoEdit.ejs", { todoTasks: tasks, idTask: id });
+        });
+    })
+    .post((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndUpdate(id, { content: req.body.content }, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
+
+// delete method
+app.route("/remove/:id").get((req, res) => {
+    const id = req.params.id;
+    TodoTask.findByIdAndRemove(id, err => {
+        if (err) return res.send(500, err);
+        res.redirect("/");
+    });
+});
+
